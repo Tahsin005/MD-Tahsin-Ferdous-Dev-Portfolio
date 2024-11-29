524c85a5ae0f64f7be21f500bfd2731fea597a5d
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-29 21:24:38 +0600

    </>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header className='header'>
       <NavLink to='/' className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
         {/* <img src={``} alt='logo' className='w-18 h-18 object-contain' /> */}
-        <p className="blue-gradient_text">TH</p>
+        <p className="blue-gradient_text">MTF</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
