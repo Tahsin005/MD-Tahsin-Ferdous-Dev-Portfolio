4fb89e27a7218417c65468d53c4e2f550a11a967
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-12-11 19:02:23 +0600

    <//>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
 
+// App.jsx
+
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
